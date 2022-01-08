e36365030b15e5a8c77d17c37750067cb9738ffd
fedeoli <lvofrc95@gmail.com>
2022-01-08 10:19:47 +0100

    deleted previous installations

--- scripts/startGedi.sh
+++ scripts/startGedi.sh
@@ -2,11 +2,9 @@
 docker run --rm -it \
 -e DISPLAY=$DISPLAY \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
+-v /home/fedeoli/Documents/WinccOA/data/proj/DemoApplication-3.17:/proj/default \
 $1 \
-
-
-#-v /home/fedeoli/Documents/WinccOA/data/proj/DemoApplication-3.17:/proj/default \
-#WCCOAui -autoreg -m gedi -config /proj/default/config/config
+WCCOAui -autoreg -m gedi -config /proj/default/config/config
 
 #-v /home/fedeoli/Documents/WinccOA/data/proj/DemoApplication-3.17/progs.ui:/proj/default/config/progs.ui \
 #-v /home/fedeoli/Documents/WinccOA/data/proj/DemoApplication-3.17/config.ui:/proj/default/config/config.ui \
